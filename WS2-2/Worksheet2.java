12698838a3560f38002779de4b9f8a1b33e8ba50
eternalxiao <forevermxl@gmail.com>
2019-01-29 20:09:12 +0000

    finish coding for WS2-2

--- WS2-2/Worksheet2.java
+++ WS2-2/Worksheet2.java
@@ -34,10 +34,11 @@ public class Worksheet2 implements Worksheet2Interface {
 	static int level(Tree<Integer> a, int x) {
 		if (a.isEmpty())
 			return 0;
-		else if(a.getValue()==x) return 1;
+		else if (a.getValue() == x)
+			return 1;
 		else
-			return (level(a.getLeft(), x) > 0 ? 1 + level(a.getLeft(), x)
-					: 0) + (level(a.getRight(), x) > 0 ? 1 + level(a.getRight(), x) : 0);
+			return (level(a.getLeft(), x) > 0 ? 1 + level(a.getLeft(), x) : 0)
+					+ (level(a.getRight(), x) > 0 ? 1 + level(a.getRight(), x) : 0);
 	}
 
 	// Exercise 4
@@ -99,22 +100,30 @@ public class Worksheet2 implements Worksheet2Interface {
 
 	// Exercise 9
 	static <E> boolean isHeightBalanced(Tree<E> a) {
-		if(a.isEmpty()) return true;
-		else return Math.abs(a.getLeft().getHeight()-a.getRight().getHeight())<=1 &&isHeightBalanced(a.getLeft()) && isHeightBalanced(a.getRight());
- 		
+		if (a.isEmpty())
+			return true;
+		else
+			return Math.abs(a.getLeft().getHeight() - a.getRight().getHeight()) <= 1 && isHeightBalanced(a.getLeft())
+					&& isHeightBalanced(a.getRight());
+
 	}
 
 	// Exercise 10
 
 	static Tree<Integer> insertHB(Tree<Integer> a, int x) {
-		if(a.isEmpty()) return new Tree<>(x);
-		else if(x<a.getValue()) return new Tree<>(a.getValue(),insertHB(a.getLeft(),x),a.getRight());
-		else return new Tree<>(a.getValue(),a.getLeft(),insertHB(a.getRight(),x));
-		
+		Tree<Integer> t = insert(x, a);
+		if (isHeightBalanced(t))
+			return t;
+		else
+			return heightBalance(t);
 	}
 
 	static Tree<Integer> deleteHB(Tree<Integer> a, int x) {
-		return new Tree<Integer>();
+		Tree<Integer> t = delete(a, x);
+		if (isHeightBalanced(t))
+			return t;
+		else
+			return heightBalance(t);
 	}
 
 	public static <E> List<E> append(List<E> a, List<E> b) {
@@ -133,13 +142,56 @@ public class Worksheet2 implements Worksheet2Interface {
 		}
 	}
 
+	public static Tree<Integer> insert(int x, Tree<Integer> a) {
+
+		if (a.isEmpty())
+			return new Tree<>(x);
+
+		else if (x < a.getValue())
+			return new Tree<>(a.getValue(), insert(x, a.getLeft()), a.getRight());
+
+		else
+			return new Tree<>(a.getValue(), a.getLeft(), insert(x, a.getRight()));
+
+	}
+
+	public static Tree<Integer> heightBalance(Tree<Integer> a) {
+		if (a.isEmpty())
+			return new Tree<>();
+		else if ((a.getRight().getHeight() - a.getLeft().getHeight()) == -2) {
+			if (Math.abs(a.getLeft().getRight().getHeight() - a.getLeft().getLeft().getHeight()) == 2)
+				return new Tree<>(a.getValue(), heightBalance(a.getLeft()), a.getRight());
+			else if ((a.getLeft().getRight().getHeight() - a.getLeft().getLeft().getHeight()) <= 0)
+				return new Tree<>(a.getLeft().getValue(), a.getLeft().getLeft(),
+						new Tree<>(a.getValue(), a.getLeft().getRight(), a.getRight()));
+			else
+				return new Tree<>(a.getLeft().getRight().getValue(),
+						new Tree<>(a.getLeft().getValue(), a.getLeft().getLeft(), a.getLeft().getRight().getLeft()),
+						new Tree<>(a.getValue(), a.getLeft().getRight().getRight(), a.getRight()));
+		} else if ((a.getRight().getHeight() - a.getLeft().getHeight()) == 2) {
+			if (Math.abs(a.getRight().getRight().getHeight() - a.getRight().getLeft().getHeight()) == 2)
+				return new Tree<>(a.getValue(), a.getLeft(), heightBalance(a.getRight()));
+			else if ((a.getRight().getRight().getHeight() - a.getRight().getLeft().getHeight()) >= 0)
+				return new Tree<>(a.getRight().getValue(),
+						new Tree<>(a.getValue(), a.getLeft(), a.getRight().getLeft()), a.getRight().getRight());
+			else
+				return new Tree<>(a.getRight().getLeft().getValue(),
+						new Tree<>(a.getValue(), a.getLeft(), a.getRight().getLeft().getLeft()), new Tree<>(
+								a.getRight().getValue(), a.getRight().getLeft().getRight(), a.getRight().getRight()));
+		} else
+			return new Tree<>(a.getValue(), heightBalance(a.getLeft()), heightBalance(a.getRight()));
+	}
+
 	public static void main(String[] args) {
 		Tree<Integer> t = new Tree<>(5, new Tree<>(3, new Tree<>(1), new Tree<>(4)),
 				new Tree<>(8, new Tree<>(6), new Tree<>(9)));
 		Tree<Integer> g = new Tree<>(1, new Tree<>(2, new Tree<>(3), new Tree<>(4)),
 				new Tree<>(5, new Tree<>(6), new Tree<>(7)));
-		Tree<Integer> f = new Tree<>(1,new Tree<>(2,new Tree<>(3),new Tree<>(4)),new Tree<>());
-		System.out.println(isHeightBalanced(f));
+		Tree<Integer> f = new Tree<>(1, new Tree<>(2, new Tree<>(3), new Tree<>(4)), new Tree<>());
+		Tree<Integer> ins = new Tree<>(100, new Tree<>(47, new Tree<>(45), new Tree<>(50)), new Tree<>(200));
+		Tree<Integer> t1;
+		t1 = insertHB(ins, 51);
+		System.out.println(t1);
 		// printDescending(t);
 	}
 }
